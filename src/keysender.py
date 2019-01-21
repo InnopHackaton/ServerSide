f47ba3f5a1b80e2ddaea100b32351b44f2794369
Vladius25 <vkorol2509@icloud.com>
2019-01-21 07:21:25 +0500

    Simplier and Better

--- src/keysender.py
+++ src/keysender.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3.6
-from socketserver import TCPServer, StreamRequestHandler
 import socket
 import logging
 
 
-class Handler(StreamRequestHandler):
-    def handle(self):
-        self.data = self.rfile.readline().strip()
-        logging.info("From <%s>: %s" % (self.client_address, self.data))
-
-        self.wfile.write(key)
-
-
-class Server(TCPServer):
-    SYSTEMD_FIRST_SOCKET_FD = 3
-
-    def __init__(self, server_address, handler_cls):
-        TCPServer.__init__(
-            self, server_address, handler_cls, bind_and_activate=False)
-
-        self.socket = socket.fromfd(
-            self.SYSTEMD_FIRST_SOCKET_FD, self.address_family, self.socket_type)
-
-
-if __name__ == "__main__":
-    with open("pubkey.asc", "rb") as keyfile:
+with open("pubkey.asc", "rb") as keyfile:
         key = keyfile.read()
-    logging.basicConfig(level=logging.INFO)
-    HOST, PORT = "localhost", 1338
-    server = Server((HOST, PORT), Handler)
-    server.serve_forever()
+
+logging.basicConfig(level=logging.INFO)
+sock = socket.socket()
+sock.bind(('', 1338))
+
+while True:
+    sock.listen(1)
+    conn, addr = sock.accept()
+    logging.info("<%s> connected" % (addr[0]))
+
+    conn.send(key)
+
+    conn.close()
+
